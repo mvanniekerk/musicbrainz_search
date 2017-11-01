668e444817585ed7a080c1244e9ea9631fbe2133
mvanniekerk <matthijsvanniekerk@gmail.com>
2017-11-01 22:21:56 +0100

    ArtistStore is now limited so it does not run out of memory

--- src/main/java/ArtistStore.java
+++ src/main/java/ArtistStore.java
@@ -2,36 +2,73 @@ import dataType.Artist;
 import lombok.NoArgsConstructor;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 
-@NoArgsConstructor
 public class ArtistStore {
 
-    private final Map<String, Artist> artists = new HashMap<>();
+    private final Map<String, Artist> artists;
+    private final int lowerID;
+    private final int higherID;
 
-    public void aggregateFromDB() throws SQLException {
-        Connection conn = MusicBrainzDB.getConnection();
+    ArtistStore(int lowerID, int higherID) {
+       artists = new HashMap<>();
+       this.lowerID = lowerID;
+       this.higherID = higherID;
+    }
 
-        Statement statement = conn.createStatement();
+    Connection getConnection() {
+        return MusicBrainzDB.getConnection();
+    }
 
-        statement.setFetchSize(500);
+    PreparedStatement getArtistAliases() throws SQLException {
+        Connection conn = getConnection();
 
-        ResultSet resultSet = statement.executeQuery(
-                "SELECT artist_alias.name, artist.gid " +
+        return conn.prepareStatement(
+            "SELECT artist_alias.name, artist.gid " +
                 "FROM artist " +
-                "LEFT JOIN artist_alias ON artist.id = artist_alias.artist"
+                "LEFT JOIN artist_alias ON artist.id = artist_alias.artist " +
+                "WHERE (artist.id >= ?) AND (artist.id < ?)"
         );
+    }
 
+    PreparedStatement getArtistCredits() throws SQLException {
+        Connection conn = getConnection();
+
+        return conn.prepareStatement(
+            "select artist_credit_name.name, artist.gid " +
+                "from artist " +
+                "left join artist_credit_name on artist_credit_name.artist = artist.id " +
+                "WHERE (artist.id >= ?) AND (artist.id < ?)"
+        );
+    }
+
+    ResultSet executePreparedStatement(PreparedStatement pstmt) throws SQLException {
+        pstmt.setInt(1, lowerID);
+        pstmt.setInt(2, higherID);
+
+        return pstmt.executeQuery();
+    }
+
+    void populateHashMap(ResultSet resultSet) throws SQLException {
         while (resultSet.next()) {
             String gid = resultSet.getString("gid");
             Artist artist = find(gid);
             String name = resultSet.getString("name");
             artist.addName(name);
         }
+    }
+
+    void aggregateFromDB() throws SQLException {
+        ResultSet artist_aliases = executePreparedStatement(getArtistAliases());
+        populateHashMap(artist_aliases);
+
+        ResultSet artist_credits = executePreparedStatement(getArtistCredits());
+        populateHashMap(artist_credits);
     }
 
     Artist find(String gid) {
@@ -46,7 +83,7 @@ public class ArtistStore {
     }
 
     public static void main(String[] args) throws SQLException {
-        ArtistStore artistStore = new ArtistStore();
+        ArtistStore artistStore = new ArtistStore(0, 500);
 
         artistStore.aggregateFromDB();
     }
--- src/main/java/MusicBrainzDB.java
+++ src/main/java/MusicBrainzDB.java
@@ -29,7 +29,6 @@ public class MusicBrainzDB {
 
         try {
             connection = DriverManager.getConnection(url, properties);
-            connection.setAutoCommit(false);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
