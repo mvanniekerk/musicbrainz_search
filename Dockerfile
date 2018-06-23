69842009ff1a50ada60ce228835566bbff50f12e
Matthijs van Niekerk <matthijsvanniekerk@gmail.com>
2018-06-23 17:51:58 +0200

    Works with aggregate commands

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ FROM openjdk:8-jre-alpine
 ADD target/musicbrainz_search-*.jar musicbrainz_search.jar
 
 EXPOSE 4567
-CMD java -jar musicbrainz_search.jar
+CMD java -jar musicbrainz_search.jar ${MB_SEARCH}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         if (args.length == 0 || args.length > 1) {
             System.out.println(HELP_TEXT);
+            return;
         }
         String arg = args[0];
 
@@ -21,6 +22,7 @@ public class Main {
                     System.out.println(SQL_ERROR_TEXT);
                     e.printStackTrace();
                 }
+                return;
             case "recording-aggregate":
                 try {
                     RecordingAggregator.aggregate();
@@ -28,10 +30,13 @@ public class Main {
                     System.out.println(SQL_ERROR_TEXT);
                     e.printStackTrace();
                 }
+                return;
             case "optimize":
                 Scoring.run();
+                return;
             case "webserver":
                 SearchController.run();
+                return;
             default:
                 System.out.println(HELP_TEXT);
         }
@@ -39,7 +44,7 @@ public class Main {
 
     private static final String HELP_TEXT =
             "Usage: musicbrainz_search [work-aggregate | recording-aggregate | optimize | webserver] \n" +
-                    "   -h      show this screen\n";
+                    "   -h      show this screen";
 
     private static final String SQL_ERROR_TEXT =
             "Cannot connect to the database";
