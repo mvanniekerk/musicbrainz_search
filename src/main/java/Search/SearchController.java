0ed9ec8ca8462213d27e6d57252c4e8c89eec2b5
Matthijs van Niekerk <matthijsvanniekerk@gmail.com>
2019-06-25 21:53:32 +0200

    Add some more test cases

--- src/main/java/Search/SearchController.java
+++ src/main/java/Search/SearchController.java
@@ -35,7 +35,7 @@ public class SearchController {
 
             String strResult;
             try {
-                strResult = new CrossFieldSearcher(1, 1, 1.75f).
+                strResult = new CrossFieldSearcher(1, 1, 1).
                         search(query, composerQuery, artistQuery, from, 20);
             } catch (IOException e) {
                 throw new RuntimeException(e);
